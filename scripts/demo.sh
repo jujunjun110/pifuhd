997134f2f65602666ee6c82d72a5ce2edf04f159
Jun Ito <jun_ito@voyagegroup.com>
2020-07-21 23:11:48 +0900

    Update: add dependencies on pipfile

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 
-python -m apps.simple_test
-# python apps/clean_mesh.py -f ./results/pifuhd_final/recon
-python -m apps.render_turntable -f ./results/pifuhd_final/recon -ww 512 -hh 512
+pipenv run python -m apps.simple_test
+# pipenv run python apps/clean_mesh.py -f ./results/pifuhd_final/recon
+pipenv run python -m apps.render_turntable -f ./results/pifuhd_final/recon -ww 512 -hh 512
